1954a95d5cae03a0de83a9efed19e4bcbac3674c
Lucasfb <lucas1214fb@gmail.com>
2022-12-03 16:45:10 -0300

    Part two solved

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -9,16 +9,25 @@ fn main() {
         let mut turn: String = strategy.split(" ").collect();
         let my_move = turn.pop().unwrap();
         let opponent_move = turn.pop().unwrap();
-        total_score = total_score + match_score(&opponent_move,&my_move);
+        total_score = total_score + match_score_part1(&opponent_move,&my_move);
     }
+    println!("Answer for Part One: {}",total_score);
 
-    println!("Answer for Part One: {}",total_score)
-
+    // Start of part two
+    let mut total_score = 0;
+    let list_matches = input.lines();
+    for strategy in list_matches {
+        let mut turn: String = strategy.split(" ").collect();
+        let my_move = turn.pop().unwrap();
+        let opponent_move = turn.pop().unwrap();
+        total_score = total_score + match_score_part2(&opponent_move,&my_move);
+    }
+    println!("Answer for Part Two: {}",total_score);
 }
 
 
 
-fn match_score(opponent_move: &char, my_move: &char) -> u32 {
+fn match_score_part1(opponent_move: &char, my_move: &char) -> u32 {
     match (opponent_move,my_move) {
         ('A','X') => 3+1,
         ('A','Y') => 6+2,
@@ -33,6 +42,20 @@ fn match_score(opponent_move: &char, my_move: &char) -> u32 {
     }
 }
 
+fn match_score_part2(opponent_move: &char, my_move: &char) -> u32 {
+    match (opponent_move,my_move) {
+        ('A','X') => 3+0,
+        ('A','Y') => 1+3,
+        ('A','Z') => 2+6,
+        ('B','X') => 1+0,
+        ('B','Y') => 2+3,
+        ('B','Z') => 3+6,
+        ('C','X') => 2+0,
+        ('C','Y') => 3+3,
+        ('C','Z') => 1+6,
+        _ => panic!()
+    }
+}
 
 fn open_input_file() -> String {
     let input_filename = "input.txt";
