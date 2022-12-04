303d3c66d876b1f374d9e0b5269a4d61ab00f267
Lucasfb <lucas1214fb@gmail.com>
2022-12-04 20:58:20 -0300

    Day 4 Part One Complete

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,6 +1,35 @@
+use std::ops::{RangeBounds, RangeInclusive};
+
 fn main() {
     println!("Advent of Code 2022 - Day 3");
+    let input = open_input_file();
+
+    let mut pair_fully_contains_count = 0;
+    for pair in input.lines() {
+        let ranges: Vec<&str> = pair.split(',').collect();
+        let first_range : Vec<&str> = ranges.get(0).unwrap().split('-').collect::<Vec<&str>>();
+        let second_range : Vec<&str> = ranges.get(1).unwrap().split('-').collect::<Vec<&str>>();
+        let (first_range_start,first_range_end) = (first_range.get(0).unwrap().parse::<usize>().unwrap(),
+                                                            first_range.get(1).unwrap().parse::<usize>().unwrap());
+        let (second_range_start,second_range_end) = (second_range.get(0).unwrap().parse::<usize>().unwrap(),
+                                                            second_range.get(1).unwrap().parse::<usize>().unwrap());
+        let first_range = first_range_start..=first_range_end;
+        let second_range = second_range_start..=second_range_end;
+
+        if ranges_fully_overlap(first_range,second_range)  {
+            pair_fully_contains_count += 1;
+        }
+       
+    }
+    println!("Answer for Part One: {}",pair_fully_contains_count);
 }
+
+fn ranges_fully_overlap(first: RangeInclusive<usize>,second: RangeInclusive<usize>) -> bool{
+    (first.contains(second.start()) && first.contains(second.end()))|| 
+    (second.contains(first.start()) && second.contains(first.end()))
+
+}
+
 
 fn open_input_file() -> String {
     let input_filename = "input.txt";
