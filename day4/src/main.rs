3827f8dd43861bc707b4f6039f7b5d7820a19677
Lucasfb <lucas1214fb@gmail.com>
2022-12-04 21:00:26 -0300

    Day 4 Part Two completed

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -22,6 +22,27 @@ fn main() {
        
     }
     println!("Answer for Part One: {}",pair_fully_contains_count);
+
+    // Start of Part Two
+
+    let mut pair_any_overlap_count = 0;
+    for pair in input.lines() {
+        let ranges: Vec<&str> = pair.split(',').collect();
+        let first_range : Vec<&str> = ranges.get(0).unwrap().split('-').collect::<Vec<&str>>();
+        let second_range : Vec<&str> = ranges.get(1).unwrap().split('-').collect::<Vec<&str>>();
+        let (first_range_start,first_range_end) = (first_range.get(0).unwrap().parse::<usize>().unwrap(),
+                                                            first_range.get(1).unwrap().parse::<usize>().unwrap());
+        let (second_range_start,second_range_end) = (second_range.get(0).unwrap().parse::<usize>().unwrap(),
+                                                            second_range.get(1).unwrap().parse::<usize>().unwrap());
+        let first_range = first_range_start..=first_range_end;
+        let second_range = second_range_start..=second_range_end;
+
+        if ranges_any_overlap(first_range,second_range)  {
+            pair_any_overlap_count += 1;
+        }
+       
+    }
+    println!("Answer for Part Two: {}",pair_any_overlap_count);
 }
 
 fn ranges_fully_overlap(first: RangeInclusive<usize>,second: RangeInclusive<usize>) -> bool{
@@ -30,6 +51,11 @@ fn ranges_fully_overlap(first: RangeInclusive<usize>,second: RangeInclusive<usiz
 
 }
 
+fn ranges_any_overlap(first: RangeInclusive<usize>,second: RangeInclusive<usize>) -> bool{
+    (first.contains(second.start()) || first.contains(second.end()))|| 
+    (second.contains(first.start()) || second.contains(first.end()))
+
+}
 
 fn open_input_file() -> String {
     let input_filename = "input.txt";
