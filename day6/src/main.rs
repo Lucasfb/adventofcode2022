bae2a79d3726813298e35f378ec0e6cdd1b4259f
Lucasfb <lucas1214fb@gmail.com>
2022-12-06 22:43:37 -0300

    Refactor using all_different_elements() I created

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,13 +1,19 @@
 use itertools::Itertools;
 
+fn all_different_elements(input: &[u8]) -> bool {
+    let initial_size = input.len();
+    let mut dedup_input = input.to_vec();
+    dedup_input.sort(); // Doesn't seem to scale well
+    dedup_input.dedup();
+    //println!("Input: {}, Dedup: {}",input,dedup_input);
+    return initial_size == dedup_input.len()
+}
+
 fn find_position_end_of_marker(input: &str,size_of_window: usize) -> Option<usize> {
     input
         .as_bytes()  //Hard to work with chars, so it's easier to use u8
         .windows(size_of_window)
-        .position(|window| window
-                                        .iter()
-                                        .unique()
-                                        .count() == size_of_window)
+        .position(|window| all_different_elements(window))
         .map(|pos|pos+size_of_window)
 }
 
@@ -19,7 +25,6 @@ fn main() {
     println!("Answer of Part One: {:?}",find_position_end_of_marker(&input, 4).unwrap());
 
     // Start of Part Two
-
     println!("Answer of Part One: {:?}",find_position_end_of_marker(&input, 14).unwrap());
   
 }
