2ce7092471636ed8d73bfe9f853a849a13a415ba
Lucasfb <lucas1214fb@gmail.com>
2022-12-06 21:41:27 -0300

    Day 6 Part Two completed

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -13,6 +13,15 @@ fn different_4_chars(a:char,b:char,c:char,d:char,) -> bool {
     a != b && a != c && a != d && b != c && b != d && c != d 
 }
 
+fn all_different_chars(input: &str) -> bool {
+    let initial_size = input.len();
+    let mut dedup_input = input.chars().map(|c| c as u8).collect::<Vec<u8>>();
+    dedup_input.sort();
+    dedup_input.dedup();
+    //println!("Input: {}, Dedup: {}",input,dedup_input);
+    return (initial_size == dedup_input.len())
+}
+
 fn main() {
     println!("Advent of Code 2022 - Day 6");
     let input = open_input_file();
@@ -35,6 +44,26 @@ fn main() {
     }
     println!("Answer of Part One: {:?}",end_of_packet_index+1);
 
+    // Start of Part Two
+
+    let mut start_of_message_found = false;
+    
+    let mut start_of_message_index = 0;
+    let mut end_of_message_index = start_of_message_index + 13;
+
+    while !start_of_message_found {
+        let try_input = input.get(start_of_message_index..=end_of_message_index).unwrap();
+        if all_different_chars(try_input) {
+            start_of_message_found = true;
+        } else {
+            start_of_message_index += 1;
+            end_of_message_index +=1; 
+        }
+        
+    }
+    println!("Answer of Part One: {:?}",end_of_message_index+1);
+
+    
 }
 
 fn open_input_file() -> String {
